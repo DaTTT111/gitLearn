168f9c4dc8819f2651751175af2ebaf707149379
DaTTT111 <wz__@163.com>
2017-11-21 17:33:34 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,12 +13,12 @@ if [ "$1" = 'kibana' ]; then
 	fi
   
   if [ "$ELASTICSEARCH_NAME" ]; then
-    echo 'elasticsearch.username: "ELASTICSEARCH_NAME"' >> /opt/kibana/config/kibana.yml
+    echo 'kibana_elasticsearch_username: ELASTICSEARCH_NAME' >> /opt/kibana/config/kibana.yml
     sed -i 's/ELASTICSEARCH_NAME/'"$ELASTICSEARCH_NAME"'/g' /opt/kibana/config/kibana.yml
   fi
   
   if [ "$ELASTICSEARCH_PASSWD" ]; then
-    echo 'elasticsearch.password: "ELASTICSEARCH_PASSWD"' >> /opt/kibana/config/kibana.yml
+    echo 'kibana_elasticsearch_password: ELASTICSEARCH_PASSWD' >> /opt/kibana/config/kibana.yml
     sed -i 's/ELASTICSEARCH_PASSWD/'"$ELASTICSEARCH_PASSWD"'/g' /opt/kibana/config/kibana.yml
   fi
 
